16acf432453f025010bbe55750dc9203dc3fd0f4
SANTOSleo <168563801+Santosleonardolatuso@users.noreply.github.com>
2024-10-21 23:38:44 +0700

    gameboard and move wumpus.js

--- wumpus.js
+++ wumpus.js
@@ -68,3 +68,101 @@ function walk(direction){
 	checkRoom();
 	score();
 }
+
+function getRandomEmptySquare(){
+	while(true){		
+		var x = Math.floor(Math.random() * 25) + 1; // Generates the gameboard
+		if((gameboard[1] != x) &&
+		(gameboard[2] != x) &&
+		(gameboard[3] != x) &&
+		(gameboard[4] != x) &&
+		(gameboard[5] != x) &&
+		(gameboard[6] != x) &&
+		(gameboard[7] != x))
+			return x;
+	}
+}
+
+function isSquareEmpty(squareNum){
+	for(var i in gameboard){ 
+		if(squareNum == gameboard[i]) {
+			return false; // The square EXISTS in the array
+		}
+	}
+	return true; // The square does NOT EXIST in the array
+}
+
+function move(to){
+	if(isSquareEmpty(to)){
+		updateUI(gameboard[1], to); // Updates where the player will go
+		gameboard[1] = to; // Stores the new position
+	}
+	else{
+		if(to == gameboard[2]){
+			redroom(gameboard[1], to);
+			alert("You are dead!! Wumpus ate you.");
+			newGame();
+		}
+		else if((to == gameboard[3]) || (to == gameboard[4])){
+			alert("The bat takes you to another part of the cave");
+			var newroom = getRandomEmptySquare();
+			updateUI(gameboard[1], newroom); 
+			gameboard[1] = newroom; 			
+		}
+		else if(to == gameboard[7]) {
+			gameboard[7] = 0;
+			updateUI(gameboard[1], to);
+			gameboard[1] = to;
+			moves = moves + 20;
+		}
+		else {
+			document.getElementById('room-' + gameboard[1]).style.backgroundColor = 'lightgreen';
+			document.getElementById('room-' + to).style.backgroundColor = 'red';
+			alert("You fall and hurt yourself badly, almost dying.");
+			newGame();
+		}
+	}	
+}
+
+function checkRoom(){
+	if(gameboard[1] % 5 != 0) {
+		//check right
+		if(gameboard[1] + 1 == gameboard[2]){
+			feedback("You smell the Wumpus!");
+		} else if ((gameboard[1] + 1 == gameboard[3]) || (gameboard[1] + 1 == gameboard[4])){
+			feedback("You hear wings flapping!");
+		} else if ((gameboard[1] + 1 == gameboard[5]) || (gameboard[1] + 1 == gameboard[6])){
+			feedback("You feel a draft from a pit!");
+		}
+	}
+	if((gameboard[1] - 1) % 5 != 0) {
+		//check left
+		if(gameboard[1] - 1 == gameboard[2]){
+			feedback("You smell the Wumpus!");
+		} else if ((gameboard[1] - 1 == gameboard[3]) || (gameboard[1] - 1 == gameboard[4])){
+			feedback("You hear wings flapping!");
+		} else if ((gameboard[1] - 1 == gameboard[5]) || (gameboard[1] - 1 == gameboard[6])){
+			feedback("You feel a draft from a pit!");
+		}
+	}
+	if((gameboard[1] - 5) > 0) {
+		//check up
+		if(gameboard[1] - 5 == gameboard[2]){
+			feedback("You smell the Wumpus!");
+		} else if ((gameboard[1] - 5 == gameboard[3]) || (gameboard[1] - 5 == gameboard[4])){
+			feedback("You hear wings flapping!");
+		} else if ((gameboard[1] - 5 == gameboard[5]) || (gameboard[1] - 5 == gameboard[6])){
+			feedback("You feel a draft from a pit!");
+		}
+	}
+	if((gameboard[1] + 5) <= 25) {
+		//check down
+		if(gameboard[1] + 5 == gameboard[2]){
+			feedback("You smell the Wumpus!");
+		} else if ((gameboard[1] + 5 == gameboard[3]) || (gameboard[1] + 5 == gameboard[4])){
+			feedback("You hear wings flapping!");
+		} else if ((gameboard[1] + 5 == gameboard[5]) || (gameboard[1] + 5 == gameboard[6])){
+			feedback("You feel a draft from a pit!");
+		}
+	}
+}
